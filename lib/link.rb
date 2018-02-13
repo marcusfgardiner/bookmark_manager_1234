d53201135e891c5ed58ae8cda451eed9dcd64d48
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-13 17:30:59 +0000

    Changed code to flexibly refer to databases

--- lib/link.rb
+++ lib/link.rb
@@ -2,13 +2,8 @@ require 'pg'
 
 class Link
   def self.all
-  
-    # if ENV['RACK_ENV'] == 'test'
-    #   connection = PG.connect dbname: ENV['CONN']
-    # else
-    #   connection = PG.connect dbname: 'bookmark_manager'
-    # end
-    #  REFACTOR to use bdname = bookmark_manager + test/dev using ENV['RACK_ENV']
+
+    connection = PG.connect dbname: 'bookmark_manager_' + ENV['RACK_ENV']
 
     result = connection.exec 'SELECT * FROM links'
 
